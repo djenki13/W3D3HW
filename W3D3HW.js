858c6aec7c2441551ca12b04a590dff999915b5c
djenki13 <danieledmundjenkins@gmail.com>
2023-10-26 14:39:50 -0400

    completed HW

---
+++ W3D3HW.js
@@ -0,0 +1,72 @@
+// Part 1
+
+// class Governor {
+//     static name = 'Dan Jenkins'
+//     static age = 31
+//     static state = 'Pennsylvania'
+
+//     static hello() {
+//         return 'Hello, I am ' +Governor.name + ', governor of ' +Governor.state +'.'
+//     }
+
+//     static elaborate() {
+//         return 'I am ' +Governor.age + ' years of age.'
+//     }
+// }
+
+// console.log(Governor.hello());
+// console.log(Governor.elaborate());
+
+// Part 2
+
+// class Person {
+//     constructor(name, age, eyes) {
+//         this.name = name
+//         this.age = age
+//         this.eyes = eyes
+//     }
+// }
+
+// class PostalWorker extends Person {
+
+// }
+
+// const postman = new PostalWorker(
+//     "Adam",
+//     37,
+//     "blue"
+// );
+
+// console.log(postman)
+
+// class Chef extends Person {
+
+// }
+
+// const Michael = new Chef(
+//     "Michael",
+//     42,
+//     "hazel"
+// );
+
+// console.log(Michael)
+
+// function createPostalWorker(name, age, eyeColor) {
+//     return new PostalWorker(name, age, eyeColor)
+// }
+
+// const worker1 = createPostalWorker('Preston', 59, 'green')
+// const worker2 = createPostalWorker('Brayden', 21, 'brown')
+
+// console.log(worker1)
+// console.log(worker2)
+
+// function createChef(name, age, eyeColor) {
+//     return new Chef(name, age, eyeColor)
+// }
+
+// const chef1 = createChef('Rebecca', 43, 'blue')
+// const chef2 = createChef('Isabelle', 27, 'hazel')
+
+// console.log(chef1)
+// console.log(chef2)
